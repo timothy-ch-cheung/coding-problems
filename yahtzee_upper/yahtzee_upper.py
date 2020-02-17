828e848fb9bd7653a92830dadb846257768b40c3
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2020-02-17 23:13:39 +0000

    add O(n) algorithm to yahtzee

--- yahtzee_upper/yahtzee_upper.py
+++ yahtzee_upper/yahtzee_upper.py
@@ -1,5 +1,16 @@
 def yahtzee_upper(dice_rolls):
     dice_rolls = sorted(dice_rolls)
+    score_dict = {}
+    for roll in dice_rolls:
+        if roll in score_dict:
+            score_dict[roll] = score_dict[roll] + roll
+        else:
+            score_dict[roll] = roll
+    return max(score_dict.values())
+
+
+def yahtzee_upper_nlogn(dice_rolls):
+    dice_rolls = sorted(dice_rolls)
     previous = None
     running_total = 0
     scores = []
