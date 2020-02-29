6d6993c55905194880093dc89d69420b4808aaf7
tcc746 <timothy-ch-cheung@users.noreply.github.com>
2020-02-29 23:16:47 +0000

    Solve Bonus 3 of smooshed morse code problem

--- Smooshed_Morse_Code_1/smooshed_morse_code.py
+++ Smooshed_Morse_Code_1/smooshed_morse_code.py
@@ -1,6 +1,19 @@
 import glob
+from enum import Enum
 
 from pathlib import Path
+
+
+class WordPair():
+    def __init__(self, word, smorse):
+        self.word = word
+        self.smorse = smorse
+
+
+class Type(Enum):
+    SMORSE = "smorse_to_word"
+    WORD = "word_to_smorse"
+
 
 MORSE = ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- " \
         "--.. ".split(" ")
@@ -17,19 +30,19 @@ def smorse(word):
     return smorse_code
 
 
-def read_file(fileName):
-    with open(fileName) as file:
-        Path("word_to_smorse/").mkdir(parents=True, exist_ok=True)
-        Path("smorse_to_word/").mkdir(parents=True, exist_ok=True)
+def read_file(file_name):
+    with open(file_name) as file:
+        Path(Type.WORD + "/").mkdir(parents=True, exist_ok=True)
+        Path(Type.SMORSE + "/").mkdir(parents=True, exist_ok=True)
         count = 0
         for idx, word in enumerate(file):
             word = word.strip()
             word_length = len(word)
             smorsed_word = smorse(word)
             smorsed_word_length = len(smorsed_word)
-            with open("smorse_to_word/" + str(smorsed_word_length) + "chars", "a+") as smorse_to_word_file:
-                smorse_to_word_file.write(smorsed_word + "=" + word + "\n")
-            with open("word_to_smorse/" + str(word_length) + "letters", "a+") as word_to_smorse_file:
+            with open(Type.SMORSE + "/" + str(smorsed_word_length) + "chars", "a+") as smorse_to_word_file:
+                smorse_to_word_file.write(word + "=" + smorsed_word + "\n")
+            with open(Type.WORD + "/" + str(word_length) + "letters", "a+") as word_to_smorse_file:
                 word_to_smorse_file.write(word + "=" + smorsed_word + "\n")
             if idx % 1000 == 0:
                 print("processed {} thousand words".format(count))
@@ -47,19 +60,23 @@ def get_words_matching_smorse(smorse):
     return results
 
 
-def get_words_matching_smorse_predicate(fun, file=None):
+def get_words_matching_smorse_predicate(fun, directory=None, file=None, string_type=None):
+    if not string_type or not directory:
+        return
     if not file:
-        files_to_search = glob.glob("smorse_to_word/*")
+        files_to_search = glob.glob(directory + "/*")
     else:
-        files_to_search = ["smorse_to_word/" + file]
+        files_to_search = [directory + "/" + file]
 
     results = []
     for files in files_to_search:
         with open(files) as current_file:
             for line in current_file:
-                word_pair = line.split("=")
-                if fun(word_pair[0]):
-                    results.append(word_pair[1].strip())
+                pair = line.strip().split("=")
+                word_pair = WordPair(pair[0], pair[1])
+                word_to_check = word_pair.word if string_type == Type.WORD else word_pair.smorse
+                if fun(word_to_check):
+                    results.append(word_pair)
     return results
 
 
@@ -74,12 +91,24 @@ def contains_15_dashes(string):
 
 
 def get_word_for_15_dash_smorse():
-    fifteen_dash_word = get_words_matching_smorse_predicate(contains_15_dashes)[0]
-    print(fifteen_dash_word)
-    return smorse(fifteen_dash_word)
+    fifteen_dash_word = \
+    get_words_matching_smorse_predicate(contains_15_dashes, directory=Type.SMORSE.value, string_type=Type.SMORSE)[0]
+    return smorse(fifteen_dash_word.word)
 
 
-print(get_word_for_15_dash_smorse())
+# print(get_word_for_15_dash_smorse())
+
+
+# Bonus 3 #
+def is_perfectly_balanced(string):
+    return string.count(".") == string.count("-")
+
+
+def get_perfectly_balanced_words_21_letter():
+    results = get_words_matching_smorse_predicate(is_perfectly_balanced, directory=Type.WORD.value, file="21letters",
+                                                  string_type=Type.SMORSE)
+    result = next(filter(lambda x: x.word != "counterdemonstrations", results))
+    return result.word
 
 
 # Bonus 3 #
@@ -96,4 +125,4 @@ def get_13_letter_word_that_is_smorse_palindrome():
 def get_four_13_character_non_occuring_smorse_sequences():
     return ""
 
-# readFile("enable1.txt")
+# read_file("enable1.txt")
--- Smooshed_Morse_Code_1/test_smooshed_morse_code_1.py
+++ Smooshed_Morse_Code_1/test_smooshed_morse_code_1.py
@@ -1,6 +1,7 @@
-from Smooshed_Morse_Code_1.smooshed_morse_code import smorse, get_words_matching_smorse, \
-    get_word_for_15_dash_smorse
+from Smooshed_Morse_Code_1.smooshed_morse_code import smorse, get_word_for_15_dash_smorse, get_perfectly_balanced_words_21_letter
 
+
+# Smorse Tests #
 
 def test_smorse_should_return_sos():
     assert smorse("sos") == "...---..."
@@ -31,3 +32,14 @@ def test_smorse_should_return_three():
 def test_bonus_2_should_return_smorse_with_15_dashes_in_row():
     sequence = get_word_for_15_dash_smorse()
     assert ("-" * 15 in sequence) == True
+    print("\n",sequence)
+
+
+# BONUS 3 # Find the 21 letter word which has perfectly balanced smorse (that is not counterdemonstrations).
+def test_bonus_3_should_return_perfectly_balanced_21_letter_word():
+    word = get_perfectly_balanced_words_21_letter()
+    sequence = smorse(word)
+    assert sequence.count(".") == sequence.count("-")
+    assert len(word) == 21
+    assert word != "counterdemonstrations"
+    print("\n",word, "=", sequence)
